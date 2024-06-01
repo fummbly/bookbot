f93d93be4dad448d6050ca44e33a4d5db3322a34
fummbly <tstone449@gmail.com>
2024-05-31 23:46:43 -0400

    Added letter counter function

--- main.py
+++ main.py
@@ -2,12 +2,28 @@ def count_words(text):
 
     words = text.split()
     print(f"Word Count: {len(words)}")
+    return words
+
+
+def count_letters(words):
+    letters = {}
+    for word in words:
+        letter_list = list(word)
+        for letter in letter_list:
+            lower_letter = letter.lower()
+            if (lower_letter in letters):
+                letters[lower_letter] += 1
+            else:
+                letters[lower_letter] = 1
+
+    print(f"Letter Dict: {letters}")
 
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        count_words(file_contents)
+        words = count_words(file_contents)
+        count_letters(words)
 
 
 main()
